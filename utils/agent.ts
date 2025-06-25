c2614c64222b48e351f6300171c4c47887027511
Jacob Ford <j@cobford.com>
2025-06-25 13:40:28 -0400

    Step 2

--- utils/agent.ts
+++ utils/agent.ts
@@ -1,4 +1,6 @@
-import { generateText } from "ai";
+import { generateText, tool } from "ai";
+import z from "zod/v4";
+import fs from "fs"; 
 
 export async function codingAgent(prompt: string) {
   const result = await generateText({
@@ -6,6 +8,36 @@ export async function codingAgent(prompt: string) {
     prompt,
     system:
       "You are a coding agent. You will be working with js/ts projects. Your responses must be concise.",
+    tools: { 
+      list_files: tool({ 
+        description:
+        "List files and directories at a given path. If no path is provided, lists files in the current directory.", 
+        inputSchema: z.object({ 
+          path: z 
+            .string() 
+            .nullable() 
+            .describe( 
+              "Optional relative path to list files from. Defaults to current directory if not provided.", 
+            ), 
+        }), 
+        execute: async ({ path: generatedPath }) => { 
+          if (generatedPath === ".git" || generatedPath === "node_modules") { 
+            return { error: "You cannot read the path: ", generatedPath }; 
+          } 
+          const path = generatedPath?.trim() ? generatedPath : "."; 
+          try { 
+            console.log(`Listing files at '${path}'`); 
+            const output = fs.readdirSync(path, { 
+              recursive: false, 
+            }); 
+            return { path, output }; 
+          } catch (e) { 
+            console.error(`Error listing files:`, e); 
+            return { error: e }; 
+          } 
+        }, 
+      }), 
+    }, 
   });
 
   return { response: result.text };
